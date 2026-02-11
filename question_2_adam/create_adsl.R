0ec1db7c6084cb5b29f7974c0c65a26ab2c847dd
RamizK01 <ramizk2001@gmail.com>
2026-02-11 17:41:15 -0500

    add derivation of age groups and trt start dates and flag

--- question_2_adam/create_adsl.R
+++ question_2_adam/create_adsl.R
@@ -28,3 +28,40 @@ ds <- convert_blanks_to_na(ds)
 ex <- convert_blanks_to_na(ex)
 ae <- convert_blanks_to_na(ae)
 lb <- convert_blanks_to_na(lb)
+
+adsl <- dm |>
+  select(-DOMAIN)
+
+# Check to ensure ADSL is ORPP (One row per patient)
+stopifnot(nrow(adsl) == length(unique(adsl$USUBJID)))
+
+adsl <- adsl |>
+  mutate(
+    AGEGR9N = case_when(
+      AGE < 18 ~ 1,
+      AGE <= 50 ~ 2,
+      TRUE ~ 3
+    ),
+    AGEGR9 = c('<18', '18 - 50', '>50')[AGEGR9N]
+  ) 
+
+ex_dtm <- ex |>
+  derive_vars_dtm(
+    dtc = EXSTDTC,
+    new_vars_prefix = 'EXST'
+  )
+
+adsl <- adsl |>
+  derive_vars_merged(
+    dataset_add = ex_dtm,
+    # Add filters to ensure that valid start date is present and that the requirements
+    # are met as per the note written in the assignment
+    # no need for "contains" placebo since EXTRT is only coded as either PLACEBO or the IP
+    # check if flag for imputation not equal to H, then do not derive population flag
+    filter_add = !is.na(EXSTDTC) & (EXDOSE > 0 | (EXDOSE == 0 & EXTRT == 'PLACEBO') & EXSTTMF == 'H'), 
+    new_vars = exprs(TRTSDTM = EXSTDTM, TRTSTMF = EXSTTMF),
+    order = exprs(EXSTDTM, EXSEQ),
+    mode = "first",
+    by_vars = exprs(STUDYID, USUBJID)
+  )
+  
