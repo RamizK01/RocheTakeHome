e91ae6b5e291290e75ec1be1680170130343f03c
RamizK01 <ramizk2001@gmail.com>
2026-02-11 16:19:37 -0500

    adding comments

--- question_1_sdtm/01_create_ds_domain.R
+++ question_1_sdtm/01_create_ds_domain.R
@@ -29,10 +29,12 @@ raw_ds <- raw_ds |>
 # Create ds object and map DSTERM
 message('Begin mapping DS columns')
 ds <- assign_no_ct(
+  # Map DSTERM from raw
   raw_dat = raw_ds,
   raw_var = 'IT.DSTERM',
   tgt_var = 'DSTERM'
   ) |>
+  # Any missing DSTERMs filled using OTHERSP
   assign_no_ct(
     raw_dat = raw_ds,
     raw_var = 'OTHERSP',
@@ -56,14 +58,14 @@ ds <- assign_no_ct(
     raw_dat = raw_ds,
     raw_var = 'IT.DSSTDAT',
     tgt_var = 'DSSTDTC',
-    raw_fmt = 'm-d-y', 
+    raw_fmt = 'm-d-y', # format for dtc var
   ) |>
   # Create DSDTC
   assign_datetime(
     raw_dat = raw_ds,
     raw_var = c('DSDTCOL', 'DSTMCOL'),
     tgt_var = 'DSDTC',
-    raw_fmt = c('m-d-y', 'H:M'),
+    raw_fmt = c('m-d-y', 'H:M'), # include hour-min time component into iso format
   ) |>
   # Create VISIT
   assign_no_ct(
@@ -77,10 +79,12 @@ ds <- assign_no_ct(
     raw_var = 'INSTANCE',
     tgt_var = 'VISITNUM',
     ct_spec = ct,
-    ct_clst = 'VISITNUM' # Use CT for VISITNUM mapping
+    ct_clst = 'VISITNUM' # Use CT for VISITNUM mapping, missing formats
   ) 
 
 # Create df with flags for DSCAT mapping 
+# Due to the complexity of its derivation, I created a seperate df to map the
+# category that we will join in after using the unique oak_id key
 dscat_map <- raw_ds |>
   mutate(DSCAT = case_when(
     IT.DSDECOD == 'Randomized' ~ 'PROTOCOL MILESTONE',
@@ -89,35 +93,42 @@ dscat_map <- raw_ds |>
   )) |>
   select(oak_id, DSCAT)
   
-
+# Load in dm domain to pull Reference Start Date
 dm <- pharmaversesdtm::dm
 
 ds <- ds |>
+  # Set key identifying variables and make slight changes to ds vars
   mutate(
     STUDYID = raw_ds$STUDY,
     DOMAIN = 'DS',
+    # Taking numeric characters from studyid and concatenating with patnum
     USUBJID = paste(gsub("\\D", "", STUDYID), patient_number, sep = '-'),
-    DSDECOD = toupper(DSDECOD),
+    DSDECOD = toupper(DSDECOD), 
+    # Creating NA for any VISITNUM that are not valid integers
     VISITNUM = if_else(
       stringr::str_detect(VISITNUM, "^[0-9]+$"),
       VISITNUM,
       NA_character_
     )
   ) |>
+  # adding in DSCAT through merge
   inner_join(dscat_map, by = 'oak_id') |>
+  # Deriving DSSEQ grouped by order of subject and respective disposition dates 
   derive_seq(
     tgt_var = "DSSEQ",
     rec_vars = c("USUBJID", "DSSTDTC", 'DSDTC')
   ) |>
+  # Deriving analysis date for dispo event
   derive_study_day(
     dm_domain = dm,
     tgdt = "DSSTDTC",
     refdt = "RFXSTDTC",
     study_day_var = "DSSTDY"
   ) |>
+  # select final DS cols
   select(STUDYID, DOMAIN, USUBJID, DSSEQ, DSTERM, DSDECOD, DSCAT, VISITNUM, VISIT, DSDTC, DSSTDTC, DSSTDY)
 
-
+# Quick checks to ensure all cols are present and no extra rows are created 
 required_cols <- c(
   "STUDYID", "DOMAIN", "USUBJID", "DSSEQ", "DSTERM",
   "DSDECOD", "DSCAT", "VISITNUM", "VISIT",
